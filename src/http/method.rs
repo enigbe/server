0540f94646fca7546c1a0c768d77b33a4ccd4e16
enigbe <ochekliyeenigbe@gmail.com>
2022-02-27 13:39:53 +0100

    feat: create modules for server and http

---
+++ src/http/method.rs
@@ -0,0 +1,12 @@
+/// HTTP methods
+pub enum Method {
+    GET,
+    POST,
+    DELETE,
+    PUT,
+    PATCH,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-struct Server {
-    addr: String,
-}
+mod server;
+mod http;
 
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr,
-        }
-    }
-
-    fn run(&self) {
-        println!("Listening on {}", self.addr)
-    }
-}
+use server::Server;
 
 fn main() {
     let addr = String::from("127.0.0.1:8080");
     let server = Server::new(addr);
+
+    // run server
     server.run();
 }
