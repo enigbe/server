e6034ec65644ad3332d49748abbbf0ce6076ea98
enigbe <ochekliyeenigbe@gmail.com>
2022-03-09 08:03:44 +0100

    feat: parse incoming request
    - parse request buffer into Request object
    - impl Debug and Display for ParseError
    
    [WIP]

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,8 +1,80 @@
 use super::method::Method;
+use std::convert::{TryFrom, From};
+use std::error::Error;
+use std::fmt::{Display, Result as FmtResult, Formatter, Debug};
+use std::str::{self, Utf8Error};
 
 /// Request struct
 pub struct  Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    // GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+        unimplemented!()
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+    
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i+1..]));
+        }
+    }
+    None
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{:?}", self.message())
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl Error for ParseError {
+
 }
