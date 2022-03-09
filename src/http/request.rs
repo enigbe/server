75c610acf96a966b3f93b0cc1aca8405b4682cc4
enigbe <ochekliyeenigbe@gmail.com>
2022-03-09 09:58:03 +0100

    feat: update request parsing

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,31 +1,46 @@
-use super::method::Method;
+use crate::http::method;
+
+use super::method::{Method, MethodError};
 use std::convert::{TryFrom, From};
 use std::error::Error;
 use std::fmt::{Display, Result as FmtResult, Formatter, Debug};
 use std::str::{self, Utf8Error};
 
 /// Request struct
-pub struct  Request {
-    path: String,
-    query_string: Option<String>,
+pub struct  Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
-        unimplemented!()
+
+        let method: Method = method.parse()?;
+        let mut query_string = None;
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
+        
+        Ok(Self {
+            path: path,
+            query_string,
+            method
+        })
     }
 }
 
@@ -34,6 +49,12 @@ impl From<Utf8Error> for ParseError {
         Self::InvalidEncoding
     }
     
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
+    }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
